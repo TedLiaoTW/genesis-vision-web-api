89a3d4c955894055ccf4b847f77aab1357ced122
r-kamenskiy <r@kamenskiy.net>
2017-08-19 23:55:15 +0300

    Nancyfx configured

---
+++ GV.Console/GenesisVisionModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GV.WebApi.Console
+{
+    public class GenesisVisionModule : Nancy.NancyModule
+    {
+        public GenesisVisionModule()
+        {
+            Get("/", args => "Hello world!");
+        }
+    }
+}
--- GV.Console/Program.cs
+++ GV.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Ipfs;
+using Microsoft.AspNetCore.Hosting;
 using System;
 using System.IO;
 
@@ -8,25 +9,13 @@ namespace GV.WebApi.Console
     {
         static void Main(string[] args)
         {
-            using (var ipfs = new IpfsClient())
-            {
-                //Name of the file to add
-                string fileName = "test.txt";
+            var host = new WebHostBuilder()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .UseKestrel()
+               .UseStartup<Startup>()
+               .Build();
 
-                //Wrap our stream in an IpfsStream, so it has a file name.
-                IpfsStream inputStream = new IpfsStream(fileName, File.OpenRead(fileName));
-
-                MerkleNode node = ipfs.Add(inputStream).Result;
-
-                Stream outputStream = ipfs.Cat(node.Hash.ToString()).Result;
-
-                using (StreamReader sr = new StreamReader(outputStream))
-                {
-                    string contents = sr.ReadToEnd();
-
-                    System.Console.WriteLine(contents); //Contents of test.txt are printed here!
-                }
-            }
+            host.Run();
         }
     }
 }
